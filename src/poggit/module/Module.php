f90083a7d2474de0a26d4f0f6da06e0b9206f58b
jazzwhistle <jdcraftmcpe@gmail.com>
2016-11-12 03:48:21 +0100

    Formatting

--- src/poggit/module/Module.php
+++ src/poggit/module/Module.php
@@ -124,41 +124,40 @@ abstract class Module {
         <link type="text/css" rel="stylesheet" href="<?= Poggit::getRootPath() ?>res/style.css">
         <link type="image/x-icon" rel="icon" href="<?= Poggit::getRootPath() ?>res/poggit.ico">
         <script>
-            
-            
-        $(function () {
-    
-    // Create mobile element
-    var mobile = document.createElement('div');
-    mobile.className = 'nav-mobile';
-    document.querySelector('#header').appendChild(mobile);
 
-    // hasClass
-    function hasClass(elem, className) {
-        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-    }
+                $(function () {
+            
+            // Create mobile element
+            var mobile = document.createElement('div');
+            mobile.className = 'nav-mobile';
+            document.querySelector('#header').appendChild(mobile);
 
-    // toggleClass
-    function toggleClass(elem, className) {
-        var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
-        if (hasClass(elem, className)) {
-            while (newClass.indexOf(' ' + className + ' ') >= 0) {
-                newClass = newClass.replace(' ' + className + ' ', ' ');
+            // hasClass
+            function hasClass(elem, className) {
+                return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
             }
-            elem.className = newClass.replace(/^\s+|\s+$/g, '');
-        } else {
-            elem.className += ' ' + className;
-        }
-    }
 
-    // Mobile nav function
-    var mobileNav = document.querySelector('.nav-mobile');
-    var toggle = document.querySelector('.navbuttons');
-    mobileNav.onclick = function () {
-        toggleClass(this, 'nav-mobile-open');
-        toggleClass(toggle, 'navbuttons-active');
-    };
-});
+            // toggleClass
+            function toggleClass(elem, className) {
+                var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+                if (hasClass(elem, className)) {
+                    while (newClass.indexOf(' ' + className + ' ') >= 0) {
+                        newClass = newClass.replace(' ' + className + ' ', ' ');
+                    }
+                    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+                } else {
+                    elem.className += ' ' + className;
+                }
+            }
+
+            // Mobile nav function
+            var mobileNav = document.querySelector('.nav-mobile');
+            var toggle = document.querySelector('.navbuttons');
+            mobileNav.onclick = function () {
+                toggleClass(this, 'nav-mobile-open');
+                toggleClass(toggle, 'navbuttons-active');
+            };
+        });
         </script>
         <?php
         $this->includeJs("std");
