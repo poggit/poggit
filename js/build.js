463701d2c92ecfdc0f9697e3225cd30ae7bf2846
jazzwhistle <jdcraftmcpe@gmail.com>
2016-11-15 12:50:40 +0100

    Everyone makes mistakes

--- js/build.js
+++ js/build.js
@@ -133,7 +133,7 @@ function confirmRepoBuilds(dialog, enableRepoBuilds) {
 function startToggleOrgs() {
     var toggleOrgs = $("#toggle-orgs");
     toggleOrgs.empty();
-    initOrg("SOF3", false).appendTo(toggleOrgs);
+    initOrg(getLoginName(), false).appendTo(toggleOrgs);
     ghApi("user/orgs", {}, "GET", function(data) {
         for(var i = 0; i < data.length; i++) {
             initOrg(data[i].login, true).appendTo(toggleOrgs);
