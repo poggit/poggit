ee88f30f38edcee14506af0f2929a199583af7c2
jazzwhistle <jdcraftmcpe@gmail.com>
2017-01-03 13:40:06 +0100

    Modified copyright notice

--- src/poggit/module/home/MemberHomePage.php
+++ src/poggit/module/home/MemberHomePage.php
@@ -2,7 +2,7 @@
 /*
  * Poggit
  *
- * Copyright (C) 2017 Poggit
+ * Copyright (C) 2016-2017 Poggit
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
