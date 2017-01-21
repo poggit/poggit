fdbc76c7d9093774b36db65f70494c19407d426d
jazzwhistle <jdcraftmcpe@gmail.com>
2017-01-21 02:04:16 +0100

    Work on Review add/delete

--- js/std.js
+++ js/std.js
@@ -301,6 +301,43 @@ function updateStatus() {
         }
     });
 }
+function addReview() {
+    var reviewauthor = $("#reviewname");
+    var relId = relId;
+    var criteria = $("#criteria");
+
+    ajax("review.admin", {
+        data: {
+            relId: relId,
+            author: reviewauthor,
+            criteria: criteria,
+            action: "add"
+        },
+        method: "POST",
+        success: function(data) {
+            alert("Added Review");
+            location = location.href;
+        }
+    });
+}
+function deleteReview() {
+    var reviewauthor = $("#reviewname");
+    var criteria = $("#criteria");
+
+    ajax("review.admin", {
+        data: {
+            name: reviewauthor,
+            relId: relId,
+            criteria: criteria,
+            action: "delete"
+        },
+        method: "POST",
+        success: function(data) {
+            alert("Deleted Review");
+            location = location.href;
+        }
+    });
+}
 
 function getRelativeRootPath() {
     return "${path.relativeRoot}";
