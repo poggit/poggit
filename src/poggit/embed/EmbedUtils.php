deed9c8211f8994f153815aa501f27ea90f14d36
SOFe <sofe2038@gmail.com>
2017-01-28 13:53:37 +0000

    Removed obsolete element IDs

--- src/poggit/embed/EmbedUtils.php
+++ src/poggit/embed/EmbedUtils.php
@@ -77,12 +77,10 @@ class EmbedUtils {
     }
 
     public static function copyable(string $label, string $value) {
-        $randId = dechex(mt_rand());
         ?>
         <div class="copied remark" style="display: none"><span>Copied to clipboard<span></div>
-        <a id="ctrlcp_<?= $randId ?>" href="#" onclick='var $this = $(this); $this.next()[0].select(); execCommand("copy"); $this.prev().css("display", "block").find("span").css("background-color", "#FF00FF").stop().animate({backgroundColor: "#FFFFFF"}, 500);'><?= $label ?>:</a>
-        <input id="cpable_<?= $randId ?>" type="text"
-            value="<?= htmlspecialchars($value, ENT_QUOTES | ENT_HTML5) ?>" size="<?= ceil(strlen($value) * 0.95) ?>"/>
+        <a href="#" onclick='var $this = $(this); $this.next()[0].select(); execCommand("copy"); $this.prev().css("display", "block").find("span").css("background-color", "#FF00FF").stop().animate({backgroundColor: "#FFFFFF"}, 500);'><?= $label ?>:</a>
+        <input type="text" value="<?= htmlspecialchars($value, ENT_QUOTES | ENT_HTML5) ?>" size="<?= ceil(strlen($value) * 0.95) ?>"/>
         <?php
     }
 }
