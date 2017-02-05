1fb2f7e80854afb3fa52c4e68f619d2b45bdf79b
SOFe <sofe2038@gmail.com>
2017-02-05 16:15:59 +0800

    Copied a bit code from NOWHERE framework

--- src/poggit/builder/NowHereProjectBuilder.php
+++ src/poggit/builder/NowHereProjectBuilder.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * Poggit
+ * Poggit + NOWHERE Plugin Workspace Framework
  *
- * Copyright (C) 2016-2017 Poggit
+ * Copyright (C) 2016-2017 Poggit and PEMapModder
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ namespace poggit\builder;
 use Phar;
 use poggit\builder\lint\BuildResult;
 use poggit\module\webhooks\repo\WebhookProjectModel;
+use poggit\utils\lang\LangUtils;
 
 class NowHereProjectBuilder extends ProjectBuilder {
     public function getName(): string {
@@ -35,7 +36,86 @@ class NowHereProjectBuilder extends ProjectBuilder {
 
     protected function build(Phar $phar, RepoZipball $zipball, WebhookProjectModel $project): BuildResult {
         $result = new BuildResult();
-        // TODO: Implement build() method.
+        
+        $info = json_decode($zipball->getContents($project->path . "nowhere.json")); // TODO check for existence
+        $NAME = $info->name;
+        $CLASS = $phar->getMetadata()["class"];
+        $BUILD_NUMBER = $phar->getMetadata()["projectBuildNumber"];
+        $VERSION = "{$info->version->major}.{$info->version->minor}-{$CLASS}#{$BUILD_NUMBER}";
+        
+        $permissions = [];
+        if($zipball->isFile($project->path . "permissions.xml")) {
+            $permissions = $this->parsePerms((new SimpleXMLElement($zipball->getContents($project->path . "permissions.xml"))), [])["children"];
+        }
+
+        $phar->setStub('<?php require_once "phar://" . __FILE__ . "/entry/entry.php"; __HALT_COMPILER();');
+        $phar->addFromString("plugin.yml", yaml_emit([
+                    "name" => $NAME,
+                    "author" => $info->author,
+                    "authors" => $info->authors ?? [],
+                    "main" => $info->main,
+                    "api" => $info->api,
+                    "depend" => $info->depend ?? [],
+                    "softdepend" => $info->softdepend ?? [],
+                    "loadbefore" => $info->loadbefore ?? [],
+                    "description" => $info->description ?? "",
+                    "website" => $info->website ?? "",
+                    "prefix" => $info->prefix ?? $NAME,
+                    "load" => $info->load ?? "POSTWORLD",
+                    "version" => $VERSION,
+                    "commands" => $info->commands ?? [],
+                    "permissions" => $permissions,
+                    "generated" => date(DATE_ISO8601)
+]));
+        $this->addDir($zipball, $phar, $project->path . "src/", "src/");
+        $this->addDir($zipball, $phar, $project->path . "entry/", "entry/");
+        $this->addDir($zipball, $phar, $project->path . "resources/", "resources/");
+        // TODO lint
         return $result;
     }
+    
+    protected function addDir(RepoZipball $zipball, Phar $phar, string $from, string $localDir) {
+        /** @type SplFileInfo $file */
+        foreach($zipball->iterator("", true) as $file => $getCont) {{
+            if(substr($file, -1) === "/" or !LangUtils::startsWith($file, $from)) continue;
+            $phar->addFromString($localDir . substr($file, strlen($from)), $getCont());
+        }
+	}
+	echo "\n";
+}
+
+function walkPerms(array $stack, array &$perms){
+	$prefix = implode(".", $stack) . ".";
+	foreach(array_keys($perms) as $key){
+		$perms[$prefix . $key] = $perms[$key];
+		unset($perms[$key]);
+		$stack2 = $stack;
+		$stack2[] = $key;
+		if(isset($perms[$prefix . $key]["children"])){
+			walkPerms($stack2, $perms[$prefix . $key]["children"]);
+		}
+	}
+}
+
+function parsePerms(SimpleXMLElement $element, array $parents){
+//	var_dump($element);
+	$prefix = "";
+	foreach($parents as $parent){
+		$prefix .= $parent . ".";
+	}
+	$description = (string) $element->attributes()->description;
+	$default = (string) $element->attributes()->default;
+	$children = [];
+	foreach($element->children() as $childName => $child){
+		$copy = $parents;
+		$copy[] = $childName;
+		$children[$prefix . $childName] = parsePerms($child, $copy);
+	}
+	return [
+		"description" => $description,
+		"default" => $default,
+		"children" => $children,
+	];
+}
+
 }
