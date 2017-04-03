276dc6f08d749646cc88db9c217aea39c0d3af47
SOFe <sofe2038@gmail.com>
2017-04-03 19:27:47 +0800

    Better keys

--- src/poggit/ci/ui/fqn/FqnListModule.php
+++ src/poggit/ci/ui/fqn/FqnListModule.php
@@ -35,13 +35,19 @@ class FqnListModule extends Module {
 
     public function output() {
         if(Poggit::getModuleName() === "fqn.yml"){
-            header("Content-Type: text/yaml");
+            header("Content-Type: text/yaml"); // TODO: we need a better query than this piece of mess (very slow)
             $query = "
             SELECT
                 CONCAT(n.name, '\\\\', kc.name) AS fqn,
                 COUNT(DISTINCT p.projectId) AS projects,
                 COUNT(DISTINCT b.buildId) AS builds,
-                GROUP_CONCAT(DISTINCT CONCAT_WS('/', r.owner, r.name, p.name)) AS pn,
+                (SELECT CONCAT_WS('/', b2.created, 'https://github.com', r2.owner, r2.name, 'commit', b2.sha) FROM builds b2
+                    JOIN projects p2 ON b2.projectId=p2.projectId JOIN repos r2 ON p2.repoId=r2.repoId
+                    WHERE b2.created = MIN(b.created) LIMIT 1) AS first_commit_url,
+                (SELECT CONCAT_WS('/', b2.created, 'https://github.com', r2.owner, r2.name, 'commit', b2.sha) FROM builds b2
+                    JOIN projects p2 ON b2.projectId=p2.projectId JOIN repos r2 ON p2.repoId=r2.repoId
+                    WHERE b2.created = MAX(b.created) LIMIT 1) AS last_commit_url,
+                GROUP_CONCAT(DISTINCT CONCAT_WS('/', r.owner, r.name, p.name)) AS usages,
                 MIN(CONCAT_WS('/', r.owner, r.name, p.name, CONCAT(IF(b.class = 1, 'dev', IF(b.class = 4, 'pr', '?')), ':', b.internal))) AS example
             FROM class_occurrences co
                 JOIN builds b ON b.buildId=co.buildId
@@ -55,9 +61,21 @@ class FqnListModule extends Module {
             foreach($rows as &$row){
                 $row["projects"] = (int) $row["projects"];
                 $row["builds"] = (int) $row["builds"];
-                $row["pn"] = explode(",", $row["pn"]);
+                $parts = explode("/", $row["commit_url"]);
+                list($row["first_created"], $row["first_commit_url"]) = $parts;
+                $parts = explode("/", $row["commit_url"]);
+                list($row["last_created"], $row["last_commit_url"]) = $parts;
+                $row["usages"] = explode(",", $row["usages"]);
             }
-            echo yaml_emit($rows);
+            echo "---\n";
+            echo "# Below are a list of all classes ever declared in a non-syntax-error non-obfuscated PHP file built in Poggit-CI.\n";
+            echo "# `fqn` is the fully-qualified class name.\n";
+            echo "# `projects` is the number of projects using this class name. If this is greater than 1, pay attention -- they may conflict with each other.\n";
+            echo "# `builds` is the number of builds using this class name. This is usually not useful.\n";
+            echo "# `first_created`, `first_commit_url`, `last_created`, `last_commit_url` are the first and last commits' date and respective URLs that whose code declares this class name\n";
+            echo "# `usages` are the projects that use this class name.";
+            echo "# `example` is an example build that uses this project\n";
+            echo substr(yaml_emit($rows), 3);
             return;
         }
         header("Content-Type: text/plain");
