e24a25a84460226c3074d4766911cc458b98297e
SOFe <sofe2038@gmail.com>
2017-04-03 18:57:15 +0800

    Update FqnListModule.php

--- src/poggit/ci/ui/fqn/FqnListModule.php
+++ src/poggit/ci/ui/fqn/FqnListModule.php
@@ -28,9 +28,53 @@ class FqnListModule extends Module {
         return "fqn.txt";
     }
 
+    public function getAllNames(): array {
+        return ["fqn.txt", "fqn.table"];
+    }
+
     public function output() {
-        $rows = MysqlUtils::query("SELECT DISTINCT CONCAT(ns.name, '\\\\', cl.name) AS fqn FROM known_classes cl LEFT JOIN namespaces ns ON cl.parent = ns.nsid WHERE ns.name IS NOT NULL ORDER BY fqn");
         header("Content-Type: text/plain");
+        if(Poggit::getModuleName() === "fqn.table"){
+            $query = "
+            SELECT
+                CONCAT(n.name, '\\\\', kc.name) AS fqn,
+                COUNT(DISTINCT p.projectId) AS projects,
+                COUNT(DISTINCT b.buildId) AS builds,
+                MIN(CONCAT_WS('/', r.owner, r.name, p.name, CONCAT(IF(b.class = 1, 'dev', IF(b.class = 4, 'pr', '?')), ':', b.internal))) AS eg,
+                GROUP_CONCAT(DISTINCT CONCAT_WS('/', r.owner, r.name, p.name)) AS pn
+            FROM class_occurrences co
+                JOIN builds b ON b.buildId=co.buildId
+                JOIN projects p ON p.projectId=b.projectId
+                JOIN repos r ON r.repoId=p.repoId
+                JOIN known_classes kc ON kc.clid=co.clid
+                JOIN namespaces n ON n.nsid=kc.parent
+            GROUP BY fqn 
+            ORDER BY projects DESC, builds DESC, UPPER(fqn) ASC";
+            $rows = MysqlUtils::query($query);
+            while(is_array($row = $result->fetch_assoc())){
+                foreach($row as $k => $v){
+                    if(!isset($cols[$k])) $cols[$k] = [$k];
+                    $cols[$k][] = $v;
+                }
+            }
+            $result->close();
+            $paddings = [];
+			foreach($cols as $k => $v){
+				$paddings[$k] = max(array_map("strlen", $v));
+			}
+			$len = array_sum($paddings) + 1 + count($cols) * 3;
+			for($i = 0; $i <= $rows; $i++){
+				if($i === 0) echo str_repeat("=", $len), PHP_EOL;
+				foreach($cols as $k => $v){
+					echo "| " . str_pad($v[$i], $paddings[$k], " ", STR_PAD_RIGHT) . " ";
+				}
+				echo "|", PHP_EOL;
+				if($i === 0) echo str_repeat("=", $len), PHP_EOL;
+			}
+			echo str_repeat("=", $len), PHP_EOL;
+            return;
+        }
+        $rows = MysqlUtils::query("SELECT DISTINCT CONCAT(ns.name, '\\\\', cl.name) AS fqn FROM known_classes cl LEFT JOIN namespaces ns ON cl.parent = ns.nsid WHERE ns.name IS NOT NULL ORDER BY fqn");
         echo "# Below is a list of classes that had once been declared in a non-syntax-error non-obfuscated PHP file built in Poggit-CI.\n";
         echo "# Visit https://poggit.pmmp.io/fqn for an interactive viewer\n";
         foreach($rows as $row) {
