60fac22098c6c6abc3c0b1862127345e7f25b559
SOFe <sofe2038@gmail.com>
2017-06-05 17:59:38 +0800

    Allow arbitrary PMMP download path for Travis builds

--- res/travisInstall.sh
+++ res/travisInstall.sh
@@ -5,6 +5,8 @@ if [ "$TRAVIS" != "true" ]; then
     exit 1
 fi
 
+PM_DL_PATH="${1:-"https://poggit.pmmp.io/get.pmmp/master"}"
+
 echo Installing pthreads 3.1.6
 pecl install channel://pecl.php.net/pthreads-3.1.6
 echo Installing weakref 0.3.2
@@ -15,7 +17,7 @@ echo | pecl install channel://pecl.php.net/yaml-2.0.0RC7
 mkdir "$TRAVIS_BUILD_DIR"/../PocketMine && cd "$TRAVIS_BUILD_DIR"/../PocketMine
 echo Installing PocketMine in $PWD
 echo Downloading PocketMine build from Poggit
-wget -O PocketMine-MP.phar https://poggit.pmmp.io/get.pmmp/master
+wget -O PocketMine-MP.phar "$PM_DL_PATH"
 mkdir plugins && wget -O plugins/PluginChecker.phar https://poggit.pmmp.io/res/PluginChecker.phar
 
 echo Downloading Poggit build
