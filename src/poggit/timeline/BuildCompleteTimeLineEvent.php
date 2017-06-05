52d9831204a77c434ce2b5c0194d1e01b780ecd4
jazzwhistle <jdcraftmcpe@gmail.com>
2017-06-05 04:30:01 +0200

    Testing

--- src/poggit/timeline/BuildCompleteTimeLineEvent.php
+++ src/poggit/timeline/BuildCompleteTimeLineEvent.php
@@ -30,7 +30,7 @@ class BuildCompleteTimeLineEvent extends TimeLineEvent {
         ?>
         <!-- TODO process this in js using BuildInfoApi -->
         <div data-eventid="<?= $this->eventId ?>" class="buildCompleteEvent">
-            <h6><?= isset($this->name) ? $this->name . " - " : "Unknown " ?> build <a href='<?= Poggit::getRootPath()?>/babs/<?= dechex($this->buildId) ?>'>&amp;<?= dechex($this->buildId) ?></a> (<?= gmdate("Y-m-d H:i:s", $this->created) ?>&nbsp;UTC)</h6>
+            <h6><?= isset($this->name) ? $this->name . " - " : "Unknown " ?> build <a href="<?= Poggit::getRootPath() ?>/babs/<?= dechex($this->buildId) ?>">&amp;<?= dechex($this->buildId) ?></a> (<?= gmdate("Y-m-d H:i:s", $this->created) ?>&nbsp;UTC)</h6>
         </div>
         <?php
     }
