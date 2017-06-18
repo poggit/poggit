6788d91da4fe4901b0c8c01a683d043c39d3416e
jazzwhistle <jdcraftmcpe@gmail.com>
2017-06-18 18:19:13 +0200

    Added search/filter section to SearchPluginsByAuthor - fixes navigation issue

--- src/poggit/release/index/SearchPluginsByAuthorPage.php
+++ src/poggit/release/index/SearchPluginsByAuthorPage.php
@@ -25,6 +25,7 @@ use poggit\Config;
 use poggit\Meta;
 use poggit\release\PluginRelease;
 use poggit\utils\internet\MysqlUtils;
+use poggit\utils\PocketMineApi;
 
 class SearchPluginsByAuthorPage extends AbstractReleaseListPage {
     private $plugins = [];
@@ -44,11 +45,13 @@ class SearchPluginsByAuthorPage extends AbstractReleaseListPage {
         $plugins = MysqlUtils::query("SELECT 
             r.releaseId, r.name, r.version, rp.owner AS author, r.shortDesc, r.projectId AS projectId, r.state AS state,
             r.flags AS flags, r.icon, UNIX_TIMESTAMP(r.creation) AS created, rp.private AS private,
-            p.framework AS framework, res.dlCount AS downloads
+            p.framework AS framework, res.dlCount AS downloads,  c.category AS cat, s.since AS spoonsince, s.till AS spoontill
             FROM releases r LEFT JOIN releases r2 ON (r.projectId = r2.projectId AND r2.creation > r.creation)
                 INNER JOIN projects p ON p.projectId = r.projectId
                 INNER JOIN repos rp ON rp.repoId = p.repoId
                 INNER JOIN resources res ON res.resourceId = r.artifact
+                INNER JOIN release_categories c ON c.projectId = p.projectId
+                INNER JOIN release_spoons s ON s.releaseId = r.releaseId
             WHERE r2.releaseId IS NULL AND $where", $type, ...$args);
         if(count($plugins) === 0) {
             throw new MainReleaseListPage(["term" => implode(" ", $authors)], <<<EOM
@@ -73,6 +76,8 @@ EOM
                 $thumbNail->author = $plugin["author"];
                 $thumbNail->iconUrl = $plugin["icon"];
                 $thumbNail->shortDesc = $plugin["shortDesc"];
+                $thumbNail->categories[] = $plugin["cat"];
+                $thumbNail->spoons[] = [$plugin["spoonsince"], $plugin["spoontill"]];
                 $thumbNail->creation = (int) $plugin["created"];
                 $thumbNail->state = (int) $plugin["state"];
                 $thumbNail->flags = (int) $plugin["flags"];
@@ -91,6 +96,51 @@ EOM
     }
 
     public function output() {
+        ?>
+        <div class="search-header">
+            <div class="release-search">
+                <div class="resptable-cell">
+                    <input type="text" class="release-search-input" id="pluginSearch" placeholder="Search">
+                </div>
+                <div class="action resptable-cell" id="searchButton">Search Releases</div>
+            </div>
+            <div class="release-search">
+                <div onclick="window.location = '<?= Meta::root() ?>plugins/authors';"
+                     class="action resptable-cell">List Authors
+                </div>
+                <div onclick="window.location = '<?= Meta::root() ?>plugins/categories';"
+                     class="action resptable-cell">List Categories
+                </div>
+            </div>
+            <div class="release-filter">
+                <input id="searchAuthorsQuery" type="text" placeholder="pmmp,poggit-orphanage,sof3"/>
+                <div class="resptable-cell">
+                    <div class="action" id="searchAuthorsButton">Search by author</div>
+                </div>
+            </div>
+            <div class="release-filter">
+                <select id="category-list" onchange="filterReleaseResults()">
+                    <option value="0" <?= isset($this->preferCat) ? "" : "selected" ?>>All Categories</option>
+                    <?php
+                    foreach(PluginRelease::$CATEGORIES as $catId => $catName) { ?>
+                        <option <?= isset($this->preferCat) && $this->preferCat === $catId ? "selected" : "" ?>
+                            value="<?= $catId ?>"><?= $catName ?></option>
+                    <?php }
+                    ?>
+                </select>
+            </div>
+            <div class="release-filter">
+                <select id="api-list" onchange="filterReleaseResults()">
+                    <option value="All API Versions" selected>All API Versions</option>
+                    <?php
+                    foreach(array_reverse(PocketMineApi::$VERSIONS) as $apiversion => $description) { ?>
+                        <option value="<?= $apiversion ?>"><?= $apiversion ?></option>
+                    <?php }
+                    ?>
+                </select>
+            </div>
+        </div>
+        <?php
         $this->listPlugins($this->plugins);
     }
 }
