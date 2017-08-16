d79f258c3f62fb264c87cc87b4d61b31f820d59d
SOFe <sofe2038@gmail.com>
2017-08-16 13:01:21 +0800

    Improved virion list

--- src/poggit/ci/ui/VirionListModule.php
+++ src/poggit/ci/ui/VirionListModule.php
@@ -19,22 +19,31 @@ namespace poggit\ci\ui;
 
 use poggit\ci\builder\ProjectBuilder;
 use poggit\Mbd;
+use poggit\Meta;
 use poggit\module\Module;
 use poggit\utils\internet\Mysql;
 
 class VirionListModule extends Module {
+    private $limit;
+
     public function getName(): string {
         return "v";
     }
 
     public function output() {
+        /** @noinspection UnnecessaryCastingInspection */
+        $this->limit = (int) ($_REQUEST["top"] ?? 10);
+
         $libs = Mysql::query("SELECT
-                projects.repoId, owner repoOwner, repos.name repoName, projectId, projects.name projectName,
+                projects.repoId, owner repoOwner, repos.name repoName, projects.projectId, projects.name projectName,
                 IFNULL(t.userProjects, 0) userProjects, IFNULL(t.userBuilds, 0) userBuilds,
-                (SELECT MAX(created) FROM builds WHERE builds.projectId = projects.projectId) lastBuildDate
+                UNIX_TIMESTAMP(b.created) lastBuildDate,
+                b.main antigen,
+                IF(LENGTH(vb.version) > 0, CONCAT('v', vb.version), 'Unknown version') lastVersion
             FROM projects INNER JOIN repos ON projects.repoId = repos.repoId
             LEFT JOIN (SELECT
                     virion_project.projectId virionProjectId,
+                    MAX(virion_build.buildId) lastVirionBuild,
                     COUNT(DISTINCT user_project.projectId) userProjects,
                     COUNT(DISTINCT user_build.buildId) userBuilds
                 FROM virion_usages
@@ -44,31 +53,50 @@ class VirionListModule extends Module {
                     INNER JOIN projects user_project ON user_build.projectId = user_project.projectId
                 GROUP BY virion_project.projectId
                 ) t ON virionProjectId = projects.projectId
+            LEFT JOIN builds b ON t.lastVirionBuild = b.buildId
+            LEFT JOIN virion_builds vb ON t.lastVirionBuild = vb.buildId
             WHERE projects.type = ? AND projects.framework = ? AND NOT repos.private
-            ORDER BY userProjects DESC, userBuilds DESC",
+            ORDER BY userProjects DESC, userBuilds DESC, lastBuildDate DESC LIMIT $this->limit",
             "is", ProjectBuilder::PROJECT_TYPE_LIBRARY, "virion");
         ?>
         <html>
         <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# object: http://ogp.me/ns/object# article: http://ogp.me/ns/article# profile: http://ogp.me/ns/profile#">
-            <?php $this->headIncludes("Poggit - Popular Virions", "The most-used virions on Poggit") ?>
-            <title>Popular Virions | Poggit</title>
+            <?php $this->headIncludes("Poggit - Popular Virions", "The most popular virions on Poggit") ?>
+            <title>Top <?= $this->limit ?> Popular Virions | Poggit</title>
         </head>
         <body>
         <?php $this->bodyHeader() ?>
         <div id="body">
-            <h1>Popular Virions</h1>
+            <h1>Top <span style="cursor: pointer; border-bottom: dashed;" onclick='window.location =
+                        "v?top=" + prompt("How many virions to display?", <?= json_encode($this->limit) ?>);'>
+                    <?= $this->limit ?></span> Popular Virions</h1>
+            <p>Read more about using virions <a href="<?= Meta::root() ?>virion" target="_blank">here</a>.</p>
             <ol>
-                <?php foreach($libs as $lib) { ?>
-                    <li>
-                        <h3><?= $lib["projectName"] ?> (<?= $lib["repoOwner"] ?>)<?php Mbd::ghLink("https://github.com/{$lib["repoOwner"]}/{$lib["repoName"]}") ?></h3>
-                        <p class="remark">Used by <?= $lib["userProjects"] ?> project(s), totally <?= $lib["userBuilds"] ?> build(s)</p>
-                    </li>
-                <?php } ?>
+                <?php
+                foreach($libs as $lib) {
+                    $this->displayLib((object) $lib);
+                }
+                ?>
             </ol>
         </div>
         <?php $this->bodyFooter() ?>
         </body>
         </html>
+        <?php
+    }
+
+    private function displayLib(\stdClass $lib) {
+        ?>
+        <li>
+            <h3>
+                <a href="<?= Meta::root() ?>ci/<?= "$lib->repoOwner/$lib->repoName/$lib->projectName" ?>"><?= $lib->projectName ?></a>
+                (<?= $lib->repoOwner ?><?= $lib->repoName !== $lib->projectName ? " / $lib->repoName" : "" ?>)
+                <?php Mbd::ghLink("https://github.com/$lib->repoOwner/$lib->repoName") ?>
+            </h3>
+            <p class="remark">Antigen: <?= $lib->antigen ?? "N/A" ?></p>
+            <p class="remark">Used by <?= $lib->userProjects ?> project(s), totally <?= $lib->userBuilds ?> build(s)</p>
+            <p class="remark">Last updated: <span class="time" data-timestamp="<?= $lib->lastBuildDate ?>"></span> (<?= $lib->lastVersion ?>)</p>
+        </li>
         <?php
     }
 }
