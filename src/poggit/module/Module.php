ea49ff63bbb607b1a01c4433a546bcd7a7edeccf
SOFe <sofe2038@gmail.com>
2017-11-10 20:58:55 +0800

    Another attempt to fix GA

--- src/poggit/module/Module.php
+++ src/poggit/module/Module.php
@@ -118,7 +118,9 @@ abstract class Module {
 //        @formatter:off
         ?>
       <script> <!-- Google Analytics -->
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create','UA-93677016-1','auto');ga('set','dimension1',sessionData.session.isLoggedIn?'true':'false');ga('send','pageview');
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create','UA-93677016-1','auto');
+        ga('send','pageview',{dimension1:sessionData.session.isLoggedIn?'true':'false'});
       </script>
         <?php
 //        @formatter:on
