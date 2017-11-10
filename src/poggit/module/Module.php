51431d72959f5af6441fd39b36dbbb97738ee3d5
SOFe <sofe2038@gmail.com>
2017-11-10 21:55:00 +0800

    It should be dimension1 not cd1 :/

--- src/poggit/module/Module.php
+++ src/poggit/module/Module.php
@@ -120,8 +120,8 @@ abstract class Module {
       <script> <!-- Google Analytics -->
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
         ga('create','UA-93677016-1','auto');
-        ga('set', 'cd1', <?= json_encode(Session::getInstance()->isLoggedIn() ? "Member" : "Guest") ?>);
-        ga('set', 'cd2', <?= json_encode(Meta::ADMLV_MAP[Meta::getAdmlv()]) ?>);
+        ga('set', 'dimension1', <?= json_encode(Session::getInstance()->isLoggedIn() ? "Member" : "Guest") ?>);
+        ga('set', 'dimension2', <?= json_encode(Meta::ADMLV_MAP[Meta::getAdmlv()]) ?>);
         ga('send','pageview');
       </script>
         <?php
