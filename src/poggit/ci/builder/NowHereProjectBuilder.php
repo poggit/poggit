d1757398ed7d544486391e4a4944e89bb70249b3
SOFe <sofe2038@gmail.com>
2018-01-01 00:23:15 +0800

    Happy new year (second year)!

--- src/poggit/ci/builder/NowHereProjectBuilder.php
+++ src/poggit/ci/builder/NowHereProjectBuilder.php
@@ -3,7 +3,7 @@
 /*
  * Poggit + NOWHERE Plugin Workspace Framework
  *
- * Copyright (C) 2016-2017 Poggit and PEMapModder
+ * Copyright (C) 2016-2018 Poggit and PEMapModder
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
