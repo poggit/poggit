58d6da042ee57c0e364c58666c5727797466064f
SOFe <sofe2038@gmail.com>
2018-02-05 02:47:30 +0800

    Added ALPHA11 phar link

--- src/poggit/utils/PocketMineApi.php
+++ src/poggit/utils/PocketMineApi.php
@@ -73,6 +73,6 @@ class PocketMineApi {
         "3.0.0-ALPHA8" => ["description" => ["AsyncTask constructor changes"], "php" => ["7.2"], "incompatible" => true, "indev" => false, "phar" => ["default" => "https://github.com/pmmp/PocketMine-MP/releases/download/api%2F3.0.0-ALPHA8/PocketMine-MP_1.7dev-83_6e5759b1_API-3.0.0-ALPHA8.phar"]],
         "3.0.0-ALPHA9" => ["description" => ["New skin API"], "php" => ["7.2"], "incompatible" => true, "indev" => false, "phar" => ["default" => "https://github.com/pmmp/PocketMine-MP/releases/download/api%2F3.0.0-ALPHA9/PocketMine-MP_1.7dev-318_716c1f29_API-3.0.0-ALPHA9.phar"]],
         "3.0.0-ALPHA10" => ["description" => ["Who Knows"], "php" => ["7.2"], "incompatible" => true, "indev" => false, "phar" => ["default" => "https://github.com/pmmp/PocketMine-MP/releases/download/1.7dev-516/PocketMine-MP_1.7dev-516_fbd04b0f_API-3.0.0-ALPHA10.phar"]],
-        "3.0.0-ALPHA11" => ["description" => ["New XP API", "ArmorInventory", "LevelProvider can be constructed without a Level", "VoxelRayTrace", "NBTStream", "TextFormat::colorize()"], "php" => ["7.2"], "incompatible" => true, "indev" => true, ],
+        "3.0.0-ALPHA11" => ["description" => ["New XP API", "ArmorInventory", "LevelProvider can be constructed without a Level", "VoxelRayTrace", "NBTStream", "TextFormat::colorize()"], "php" => ["7.2"], "incompatible" => true, "indev" => true, "phar" => ["default" => "https://github.com/pmmp/PocketMine-MP/releases/download/api%2F3.0.0-ALPHA11/PocketMine-MP_1.7dev-677_07bf1c9e_API-3.0.0-ALPHA11.phar"]],
     ];
 }
