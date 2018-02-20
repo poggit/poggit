bd58d6d8ff026e1668ed3ebbfe9033dbf191d856
SOFe <sofe2038@gmail.com>
2018-02-20 12:14:26 +0800

    Fine, 24 hours.

--- src/poggit/Config.php
+++ src/poggit/Config.php
@@ -38,7 +38,7 @@ class Config {
     const MIN_CHANGELOG_LENGTH = 10;
 
     const MAX_WEEKLY_BUILDS = 60;
-    const RECENT_BUILDS_RANGE = 43200;
+    const RECENT_BUILDS_RANGE = 86400;
     const MIN_PUBLIC_RELEASE_STATE = Release::STATE_VOTED;
     const MIN_DEV_STATE = Release::STATE_VOTED; // minimum state required to get development builds shipping
     const VOTED_THRESHOLD = 5;
