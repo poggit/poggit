58b16a38fa8a50680a030c7e5d70ba0fc2445b80
SOFe <sofe2038@gmail.com>
2019-01-18 14:11:57 +0800

    Forgot to delete a line in populate.sh

--- travis/populate.sh
+++ travis/populate.sh
@@ -24,4 +24,3 @@ cat \
 		submit_rule.sql \
 	| docker exec -i `docker-compose ps -q mysql` \
 		bash -c 'mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE"' \
-	< `dirname "$0"`/populate.sql
