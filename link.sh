db1b251b222cb8cca6aff1f5930bfaaaae279816
SOFe <sofe2038@gmail.com>
2019-05-22 15:07:04 +0800

    Fixed npm links

--- link.sh
+++ link.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
-cd "`dirname "$0"`"
-./lib/link.sh
 
-cd webhook
+set -x
+
+PROJ_DIR="$(realpath "$(dirname "$0")")"
+
+cd "$PROJ_DIR"/lib/all
+npm install
+npm link
+
+cd "$PROJ_DIR"/lib/frontend
+npm install
+npm link
+npm link poggit-eps-lib-all
+
+cd "$PROJ_DIR"/lib/server
+npm install
+npm link
+npm link poggit-eps-lib-all
+
+cd "$PROJ_DIR"/webhook
+npm install
 npm link poggit-eps-lib-server
 
-cd ../app/server
+cd "$PROJ_DIR"/app/server
+npm install
 npm link poggit-eps-lib-server
 npm link poggit-eps-lib-frontend
 
-cd ../app/client
+cd "$PROJ_DIR"/app/client
+npm install
 npm link poggit-eps-lib-frontend
+
+cd "$PROJ_DIR"/tools
+npm install
