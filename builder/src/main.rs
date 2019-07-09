2e29cd0f17df2244a1eff61c03e0973f46784bfd
SOFe <sofe2038@gmail.com>
2019-07-09 12:44:35 -0400

    implemented GitHub hook verification

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -14,23 +14,46 @@
 // You should have received a copy of the GNU Affer General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#![feature(decl_macro, proc_macro_hygiene)]
+#![feature(decl_macro, proc_macro_hygiene, try_trait)]
 
 #[macro_use] extern crate common;
+extern crate hex;
+#[macro_use] extern crate lazy_static;
+extern crate ring;
 #[macro_use] extern crate rocket;
-extern crate rocket_contrib;
 extern crate serde;
 #[macro_use] extern crate serde_derive;
 
 use common::config::Config;
+use ring::{digest, hmac};
+use rocket::request::State;
+
+mod payload;
+
+#[post("/", data = "<payload>")]
+fn endpoint(payload: payload::WebhookPayload) -> String {
+    "Not implemented".into()
+}
+
+pub struct WebhookKey(hmac::VerificationKey);
+
+impl WebhookKey {
+    pub fn new(config: &Config) -> Self {
+        Self(hmac::VerificationKey::new(&digest::SHA1, config.github.webhook.secret.as_bytes()))
+    }
+}
 
 fn main() {
     common::init();
+    let config = Config::new();
+    let webhook_key = WebhookKey::new(&config);
     let server = rocket::ignite()
         .mount("/", routes![
+               endpoint,
         ])
-        .manage(Config::new());
-    info!("Starting builxer server");
+        .manage(config)
+        .manage(webhook_key);
+    info!("Starting builder server");
     let err = server.launch();
     panic!("{}", err);
 }
---
+++ builder/src/payload/mod.rs
@@ -0,0 +1,63 @@
+// Poggit
+// Copyright (C) Poggit
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affer General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use std::io::Read;
+
+use ring::hmac;
+use rocket::data::{Data, FromDataSimple, Outcome};
+use rocket::http::Status;
+use rocket::request::{Request, State};
+
+pub mod push;
+
+pub enum WebhookPayload {
+    Push(push::Payload),
+}
+
+impl FromDataSimple for WebhookPayload {
+    type Error = String;
+
+    fn from_data(request: &Request, data: Data) -> Outcome<Self, String> {
+        let sig = match request.headers().get_one("X-Hub-Signature") {
+            Some(sig) => sig,
+            None => { return Outcome::Failure((Status::BadRequest, "Missing X-Hub-Signature".into())); }
+        };
+        if &sig[0..5] != "sha1=" { return Outcome::Failure((Status::BadRequest, "Expected sha1 signature".into())); }
+        let sig = match hex::decode(&sig[5..]) {
+            Ok(vec) => vec,
+            Err(_) => { return Outcome::Failure((Status::BadRequest, "Bad signature".into())); }
+        };
+
+        let mut buf = Vec::<u8>::new();
+        if let Err(err) = data.open().take(1048576).read_to_end(&mut buf) {
+            return Outcome::Failure((Status::InternalServerError, format!("{}", err)));
+        }
+
+        let key = request.guard::<State<crate::WebhookKey>>().expect("WebhookKey not defined");
+        if let Err(_) = hmac::verify(&key.0, &buf[..], &sig[..]) {
+            return Outcome::Failure((Status::BadRequest, "Incorrect signature".into()));
+        }
+
+        let event = match request.headers().get_one("X-GitHub-Event") {
+            Some(event) => event,
+            None => {
+                return Outcome::Failure((Status::BadRequest, "Missing X-GitHub-Event".into()));
+            }
+        };
+
+        Outcome::Failure((Status::InternalServerError, "Not implemented yet".into()))
+    }
+}
