5e872ec0811f914c44888fd14d48d23f67043728
Jack Honour <JaxkDev@gmail.com>
2023-06-02 17:42:20 +0100

    Update PHPStan (0.5.1)

--- src/poggit/ci/builder/DefaultProjectBuilder.php
+++ src/poggit/ci/builder/DefaultProjectBuilder.php
@@ -415,7 +415,7 @@ class DefaultProjectBuilder extends ProjectBuilder {
 
                 $pluginPath = escapeshellarg($pluginPath);
 
-                Lang::myShellExec("docker create --cpus=1 --memory=256M -e PLUGIN_PATH={$pluginPath} --name {$id} pmmp/poggit-phpstan:0.5.0-pm$api", $stdout, $stderr, $exitCode);
+                Lang::myShellExec("docker create --cpus=1 --memory=256M -e PLUGIN_PATH={$pluginPath} --name {$id} pmmp/poggit-phpstan:0.5.1-pm$api", $stdout, $stderr, $exitCode);
 
                 if($exitCode !== 0) {
                     $status = new PhpstanInternalError();
